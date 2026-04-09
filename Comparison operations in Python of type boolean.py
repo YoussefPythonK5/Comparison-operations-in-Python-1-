0c009b86195712b4d35e7f722e185eb467339ea8
Youssef <ya103857@gmail.com>
2026-04-09 11:33:05 +0200

    Add files via upload

---
+++ Comparison operations in Python of type boolean.py
@@ -0,0 +1,13 @@
+# ==
+result = (10 == 11)
+print (result)
+result = (20 == 20)
+print (result)
+result = (5 == 3)
+print (result)
+result = (4 == 4)
+print (result)
+result = (11 == 11)
+print (result)
+
+
